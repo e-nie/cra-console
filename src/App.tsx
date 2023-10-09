135ed6bf862d045ca480af7f5fffb38e382137f6
Eva Niehaus <eniehaus111@gmail.com>
2023-10-09 21:57:20 +0200

    testing dev branch1

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className = "App">
-            <header className = "App-header">
-                <img src = {logo} className = "App-logo" alt = "logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <a
-                    className = "App-link"
-                    href = "https://reactjs.org"
-                    target = "_blank"
-                    rel = "noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <p>11</p>
-            <p>12</p>
-            <p>13</p>
-            <h1>NEW DEV - EVA-NIE!!!</h1>
-            <h1>NEW DEV -  git pull!!</h1>
-            <h1>NEW DEV - git pull!!!</h1>
-            <h1>NEW DEV - !!!</h1>
-            <h1>NEW DEV - git pull!!!</h1>
-            <h1>NEW DEV - git pull!!!</h1>
-            <h1>NEW DEV - git pull!!!</h1>
-            <h3>old dev e-nie is here again!!!</h3>
-            <div>O-))-OOOOOOOOOOOOOOOO</div>
-
+            Testing dev branch
         </div>
     );
 }
